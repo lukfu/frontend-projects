84de3814becc80317b11751794756cc0acc0f82d
lukfu <fu.lukas99@gmail.com>
2025-07-09 16:27:27 +0200

    20250609

---
+++ javascript-course/scripts/12-todo-list.js
@@ -0,0 +1,46 @@
+const todoList = [{name: 'a', dueDate:'2022-12-12'}];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach((todoObject, index) => {
+    const { name, dueDate } = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${index}, 1);
+      renderTodoList();
+    " class = "delete-todo-button">Delete</button>`;  /*generate html with `` */
+    todoListHTML += html;
+  });
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+}
+
+/*
+general JS process
+1. save data
+2. generate HTML
+3. make it interactive
+*/
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const name = inputElement.value;
+  const dueDate = dateInputElement.value
+  todoList.push({
+    /*name: name, 
+    dueDate: dueDate*/
+    name,
+    dueDate /*shorthand if name of variable and name of const is the same */
+  });
+
+  inputElement.value = '';
+
+  renderTodoList();
+}
