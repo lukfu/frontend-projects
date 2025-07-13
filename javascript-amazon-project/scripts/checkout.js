ba4ecb5dd00f1fd755e57e35a280e782369c8358
lukfu <fu.lukas99@gmail.com>
2025-07-13 16:38:10 +0200

    20250713

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,7 +1,10 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
-cart.forEach(() => {
+let cartSummaryHTML = '';
+
+cart.forEach((cartItem) => {
   const productId = cartItem.productId;
 
   let matchingProduct;
@@ -12,31 +15,34 @@ cart.forEach(() => {
     }
   });
 
+  cartSummaryHTML += 
   `
-    <div class="cart-item-container">
+    <div class="cart-item-container
+    js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
 
     <div class="cart-item-details-grid">
       <img class="product-image"
-        src="images/products/athletic-cotton-socks-6-pairs.jpg">
+        src="${matchingProduct.image}">
 
       <div class="cart-item-details">
-        <div class="product-name">
+        <div class="${matchingProduct.name}">
           Black and Gray Athletic Cotton Socks - 6 Pairs
         </div>
         <div class="product-price">
-          $10.90
+          $${formatCurrency(matchingProduct.priceCents)}
         </div>
         <div class="product-quantity">
           <span>
-            Quantity: <span class="quantity-label">2</span>
+            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
           </span>
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link"
+            data-product-id = "${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -49,7 +55,7 @@ cart.forEach(() => {
         <div class="delivery-option">
           <input type="radio" checked
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -62,7 +68,7 @@ cart.forEach(() => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Wednesday, June 15
@@ -75,7 +81,7 @@ cart.forEach(() => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-$${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Monday, June 13
@@ -88,5 +94,18 @@ cart.forEach(() => {
       </div>
     </div>
   </div>
-  `
+  `;
 });
+
+  document.querySelector('.js-order-summary')
+    .innerHTML = cartSummaryHTML;
+
+  document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+      link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
+      });
+    });
